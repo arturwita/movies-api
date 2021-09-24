b9347079c7e5d44079835e46efb11f070b431851
arturwita <a.wita@fireup.pro>
2021-09-24 21:02:46 +0200

    added CORS

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import { Container } from "./src/dependency-injection";
 import { moviesUrl } from "./src/router";
 
 const { app, config, logger, movieRouter, loggerMiddleware, errorHandler } = new Container(express()).cradle();
 
 app.use(express.json());
+app.use(cors());
 app.use(loggerMiddleware.use.bind(loggerMiddleware));
 app.use(moviesUrl, movieRouter.getRouter());
 app.use(errorHandler.use.bind(errorHandler));
